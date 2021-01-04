0a6ab8ffe8c585d10b45ec38f01a462c4f781305
Svetoslav Denev <ssd0926@gmail.com>
2021-01-04 16:33:35 +0200

    remove redundatn project

--- src/Web/API-React-Template.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MVCTemplate.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
-    }
-}
